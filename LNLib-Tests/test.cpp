e70956bd560d39d0bf06e744331ea52e049e0882
haog <2101627978@qq.com>
2023-06-18 17:16:41 +0800

    add tests

---
+++ LNLib-Tests/test.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "XYZ.h"
+#include "XYZW.h"
+#include "NurbsCurve.h"
+using namespace LNLib;
+TEST(TestLNLib, GetPointOnCurve)
+{
+	constexpr auto degree = 3;
+	std::vector<XYZW> ctrlPoints
+	{
+		{ -4, -4, 0, 1},
+		{ -2,4,0,1 },
+		{ 2,-4,0,1 },
+		{ 4,4,0,1 }
+	};
+	std::vector<double>knots{0, 0, 0, 0, 1, 1, 1, 1};
+	XYZ point{};
+	NurbsCurve::GetPointOnCurve(degree, knots, ctrlPoints, 0, point);
+	EXPECT_TRUE(point.IsAlmostEqualTo({ -4,-4, 0}));
+}
