cb3d8a2e6e445fbf83e6ff2bda8275bb07b58756
1516561950@qq.com <1516561590@qq.com>
2023-07-05 09:53:54 +0800

    提交单元测试用例

--- test/test.cpp
+++ test/test.cpp
@@ -18,3 +18,23 @@ TEST(TestLNLib, GetPointOnCurve)
 	NurbsCurve::GetPointOnCurve(degree, knots, ctrlPoints, 0, point);
 	EXPECT_TRUE(point.IsAlmostEqualTo({ -4,-4, 0}));
 }
+
+TEST(TestLNLib, GetParamOnCurve)
+{
+	constexpr auto degree = 3;
+	std::vector<XYZW> ctrlPoints
+	{
+		{ 0, 0, 0, 1},
+		{ 0,1,0,1 },
+		{ 1,1,0,1 },
+		{ 1,0,0,1 },
+		{ 1,1,0,1 },
+		{ 2,1,0,1 },
+		{ 2,0,0,1 },
+	};
+	std::vector<double>knots{0, 0, 0, 0, 1, 1, 1, 2,2,2,2};
+	XYZ point{};
+	NurbsCurve::GetPointOnCurve(degree, knots, ctrlPoints, 0.975, point);
+	double param = NurbsCurve::GetParamOnCurve(degree, knots, ctrlPoints, 0, point);
+	EXPECT_TRUE(abs(param - 0.975) < DoubleEpsilon);
+}
