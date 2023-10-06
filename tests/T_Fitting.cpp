618260ccdbf0184f0fb5bee6b98d78a110923be0
BIMCoderLiang <bim.frankliang@foxmail.com>
2023-10-06 16:51:35 +0800

    update fitting test codes.

--- tests/T_Fitting.cpp
+++ tests/T_Fitting.cpp
@@ -12,10 +12,20 @@ using namespace LNLib;
 TEST(Test_Fitting, Interpolation)
 {
 	{
+		int degree = 3;
 		std::vector<XYZ> Q = { XYZ(0,0,0),XYZ(3,4,0),XYZ(-1,4,0),XYZ(-4,0,0),XYZ(-4,-3,0) };
 		auto params = Interpolation::GetChordParameterization(Q);
-		auto result = Interpolation::ComputeKnotVector(3, params);
+		auto result = Interpolation::ComputeKnotVector(degree, params);
 		EXPECT_TRUE(MathUtils::IsAlmostEqualTo(result[4], 28.0/51));
+	}
+
+	{
+		int degree = 3;
+		std::vector<XYZ> Q = { XYZ(0,0,0),XYZ(3,4,0),XYZ(-1,4,0),XYZ(-4,0,0),XYZ(-4,-3,0) };
+		std::vector<double> kv;
+		std::vector<XYZW> cps;
+		NurbsCurve::GlobalInterpolation(degree, Q, kv, cps);
+		EXPECT_TRUE(cps.size() == Q.size());
 	}
 }
 
