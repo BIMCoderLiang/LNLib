5c753d77f9c2503c925c97af0bc9760f79c31067
BIMCoderLiang <bim.frankliang@foxmail.com>
2024-01-27 19:50:51 +0800

    add new Test file.

---
+++ tests/T_Additional.cpp
@@ -0,0 +1,14 @@
+﻿#include "gtest/gtest.h"
+#include "NurbsCurve.h"
+#include "NurbsSurface.h"
+#include "XYZ.h"
+#include "XYZW.h"
+#include "MathUtils.h"
+#include "LNObject.h"
+
+using namespace LNLib;
+
+TEST(Test_Addintional, All)
+{
+	// to be continued...
+}
