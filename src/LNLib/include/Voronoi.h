2b2decaf56759a2dcdb4d2f62bf22d3b3b8a40a0
BIMCoderLiang <bim.frankliang@foxmail.com>
2024-04-06 17:22:31 +0800

    fix: encoding error.

--- src/LNLib/include/Voronoi.h
+++ src/LNLib/include/Voronoi.h
@@ -24,7 +24,7 @@ email: shaneosullivan1@gmail.com
  * Author:
  * 2024/04/06 - Yuqing Liang (BIMCoder Liang)
  * bim.frankliang@foxmail.com
- * Î¢ÐÅ¹«ÖÚºÅ£ºBIMCoderÁºÀÏÊ¦
+ * Î¢ï¿½Å¹ï¿½ï¿½ÚºÅ£ï¿½BIMCoderï¿½ï¿½ï¿½ï¿½Ê¦
  *
  * Use of this source code is governed by a GPL-3.0 license that can be found in
  * the LICENSE file.
